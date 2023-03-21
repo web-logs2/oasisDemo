695d0685b65914b4f4dbfec97566a23bf699a779
shihui <yiqianyao.yqy@alibaba-inc.com>
2023-03-21 15:00:37 +0800

    feat: grid data add drag

--- docs/basic/src/gridData.tsx
+++ docs/basic/src/gridData.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-type RectType = 'grass' | 'sand' | 'ground' | 'rock';
+type RectType = 'grass' | 'sand' | 'ground' | 'rock' | 'forbid';
 interface IRect {
     id: string;
     type: RectType;
 }
-
 
 
 function defaultData(size: number) {
@@ -20,7 +19,7 @@ function defaultData(size: number) {
     return rects;
 }
 
-let type = 'ground'
+let type = 'grass'
 const size = 36;
 const halfSize = size / 2;
 const rectSize = 200 / size;
@@ -28,7 +27,7 @@ const padding = 1;
 const rects = defaultData(size);
 
 export default () => {
-    const [rectType, setRectType] =  useState('ground');
+    const [rectType, setRectType] =  useState(type);
 
     function outputData() {
         const outData = rects.filter(rect => rect.type && rect.type !== 'ground');
@@ -37,30 +36,79 @@ export default () => {
 
     function setType(index: number) {
         const rect = rects[index];
-        rect.type = type;
+        if(rect) {
+            rect.type = type;
+        }
     }
 
     useEffect(() => {
         const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        const coverCanvas = document.getElementById('coverCanvas') as HTMLCanvasElement;
         const canvasSize = 200;
         const canvasStyleSize = 400;
         const canvasScale = canvasStyleSize / canvasSize;
         canvas.width = canvasSize;
         canvas.height = canvasSize;
+        coverCanvas.width = canvasSize;
+        coverCanvas.height = canvasSize;
        
         const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+        const coverCtx = coverCanvas.getContext('2d') as CanvasRenderingContext2D;
 
         draw();
-        
-        canvas.onclick = (e) => {
-            const [dataX, dataY] = xy2DataXY(e.offsetX, e.offsetY);
-            // const rect = rects[dataX * (size) + dataY];
-            // rect.type = type;
-            const index = dataX * (size) + dataY;
-            setType(index);
+
+        // 在用户操作之后、更新数据以及重绘画布
+        function select(bounds: number[]) {
+            const [startDataX, startDataY, endDataX, endDataY] = bounds;
+            const minDataX = Math.min(startDataX, endDataX);
+            const maxDataX = Math.max(startDataX, endDataX);
+            const minDataY = Math.min(startDataY, endDataY);
+            const maxDataY = Math.max(startDataY, endDataY);
+            for(let i = minDataX; i <= maxDataX; i++) {
+                for(let j = minDataY; j <= maxDataY; j++) {
+                    const index = i * (size) + j;
+                    setType(index);
+                }
+            }
             draw();
         }
 
+        // 事件监听 - 触发机制
+        canvas.onmousedown = (e) => {
+            const { offsetX: startOffsetX, offsetY: startOffsetY } = e;
+            const [startDataX, startDataY] = xy2DataXY(e.offsetX, e.offsetY);
+            canvas.onmousemove = (e) => {
+                const { offsetX: endOffsetX, offsetY: endOffsetY } = e;
+                drawCover([startOffsetX, startOffsetY, endOffsetX, endOffsetY])
+            }
+            canvas.onmouseleave = done;
+            canvas.onmouseup = done;
+            
+
+            function done(e) {
+                canvas.onmouseup = null;
+                canvas.onmouseleave = null;
+                canvas.onmousemove = null;
+                const [endDataX, endDataY] = xy2DataXY(e.offsetX, e.offsetY);
+                select([startDataX, startDataY, endDataX, endDataY]);
+                drawCover();
+            }
+        }
+
+        // 绘制拾取的辅助线
+        function drawCover(coverRects?: number[]) {
+            coverCtx.clearRect(0, 0, canvasSize, canvasSize);
+            if(!coverRects) return;
+            const [startOffsetX, startOffsetY, endOffsetX, endOffsetY] = coverRects;
+            const x = startOffsetX / window.devicePixelRatio;
+            const y = startOffsetY / window.devicePixelRatio;
+            const width = (endOffsetX - startOffsetX) / window.devicePixelRatio;
+            const height = (endOffsetY - startOffsetY) / window.devicePixelRatio;
+            coverCtx.strokeRect(x, y, width, height);
+            coverCtx.stroke();
+        }
+
+        // 绘制数据
         function draw() {
             ctx.clearRect(0, 0, 200, 200);
             drawRects(rects);
@@ -68,7 +116,6 @@ export default () => {
         }
 
         function drawRects(rects: any[]) {
-            // console.log(rects[0])
             rects.forEach(rect => {
                 const { id, type = 'ground' } = rect;
                 const [x, y] = id2CanvasXY(id);
@@ -100,6 +147,8 @@ export default () => {
                     return '#cc0';
                 case 'ground':
                     return '#ccc';
+                case 'forbid':
+                    return '#f00';
                 case 'rock':
                     return '#555';
             }
@@ -121,6 +170,7 @@ export default () => {
             const dataY = Math.floor((y / canvasScale)/rectSize);
             return [dataX, dataY];
         }
+
         function xy2idXY(x: number, y: number) {
             // 0 -> +400 =>
             // 0 -> +200 =>
@@ -134,31 +184,39 @@ export default () => {
     
 
 
-    return <div style={{ height: '400px' }}>
+    return <div style={{ height: '400px', position: 'relative' }}>
         <canvas id="canvas" style={{
             width: '400px',
             height: '100%',
             outline: 'none',
         }}/>
+        <canvas id="coverCanvas" style={{
+            width: '400px',
+            height: '100%',
+            outline: 'none',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            pointerEvents: 'none',
+        }}></canvas>
         <div style={{float: 'right'}}>
             {rectType}
             <br />
-            <div style={{margin: '10px', width: '50px', height: '20px', background: '#0c0', textAlign: 'center', lineHeight:'20px'}} onClick={() => {
-                setRectType('grass');
-                type = 'grass';
-            }}>grass</div>
-            <div style={{margin: '10px', width: '50px', height: '20px', background: '#cc0', textAlign: 'center', lineHeight:'20px'}} onClick={() => {
-                setRectType('sand')
-                type = 'sand';
-            }}>sand</div>
-            <div style={{margin: '10px', width: '50px', height: '20px', background: '#ccc', textAlign: 'center', lineHeight:'20px'}} onClick={() => {
-                setRectType('ground')
-                type = 'ground';
-            }}>ground</div>
-            <div style={{margin: '10px', width: '50px', height: '20px', background: '#555', textAlign: 'center', lineHeight:'20px'}} onClick={() => {
-                setRectType('rock')
-                type = 'rock';
-            }}>rock</div>
+            {
+                [
+                    {  gridType: 'grass', color: '#0c0' },
+                    {  gridType: 'sand', color: '#cc0' },
+                    {  gridType: 'ground', color: '#ccc' },
+                    {  gridType: 'forbid', color: '#f00' },
+                    {  gridType: 'rock', color: '#555' },
+                ].map((item, index) => {
+                    const{ gridType, color } = item;
+                    return  <div key={index} style={{margin: '10px', width: '50px', height: '20px', background: color, textAlign: 'center', lineHeight:'20px'}} onClick={() => {
+                        setRectType(gridType);
+                        type = gridType;
+                    }}>{gridType}</div>
+                })
+            }
             <button onClick={() => {
                 outputData()
             }}> export </button>
